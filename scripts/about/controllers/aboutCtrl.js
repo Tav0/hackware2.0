94b1da0a549a4085f17dd42b2388cc6ac849f5b0
Tav0 <lv23@vt.edu>
2016-02-17 12:18:00 -0500

    example to show jeroen

--- scripts/about/controllers/aboutCtrl.js
+++ scripts/about/controllers/aboutCtrl.js
@@ -1,9 +1,11 @@
 (function() {
 	'use strict';
 	angular.module('about').controller('aboutCtrl', aboutCtrl);
-	aboutCtrl.$inject = ['$scope'];
+	aboutCtrl.$inject = ['$scope', '$window'];
 
-	function aboutCtrl($scope){
-		$scope.about = "About";
+	function aboutCtrl($scope, $window){
+		$scope.link = function() {
+			$window.location.href = 'https://google.com';
+		};
 	}
 }());
