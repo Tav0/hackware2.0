cf83060456bdf18deddee4d28ccfaa49854c6d0b
penguinologist <jgem2u2@gmail.com>
2016-02-18 21:22:08 -0500

    Modified the About page to include links to social media.

--- scripts/contact/controllers/contactCtrl.js
+++ scripts/contact/controllers/contactCtrl.js
@@ -5,5 +5,23 @@
 
 	function contactCtrl($scope){
 		$scope.contact = "contact";
+
+		$scope.facebook = 'https://www.facebook.com/HackWare-426489770840939/';
+        $scope.twitter = 'https://twitter.com/hack_ware';
+        $scope.email = 'mailto:admin@hackware.io';
+
+
+        $scope.linkModelFunc = function (url){
+			//console.log('link model function');
+			$window.location = url;
+		}
+        $scope.linkModelFunc1 = function (url){
+            //console.log('link model function');
+            $window.location = url;
+        }
+        $scope.linkModelFunc2 = function (url){
+            //console.log('link model function');
+            $window.location = url;
+        }
 	}
 }());
