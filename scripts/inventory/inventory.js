4803b8f3fc28307099a6f676282d779a87646504
penguinologist <jgem2u2@gmail.com>
2016-02-24 18:00:57 -0500

    updates

--- scripts/inventory/inventory.js
+++ scripts/inventory/inventory.js
@@ -1,18 +1,32 @@
 (function () {
 	'use strict';
 	angular.module('inventory', ['ngResource'])
-	.config(function($stateProvider){
+		.config(function($stateProvider){
 
-		var inventory = {
-			url: '/inventory',
-			views: {
-				'main@home' : {
-					controller : 'inventoryCtrl',
-					templateUrl: 'scripts/inventory/partials/inventory.html'
+			var inventory = {
+				url: '/inventory',
+				views: {
+					'main@home' : {
+						controller : 'inventoryCtrl',
+						templateUrl: 'scripts/inventory/partials/inventory.html'
+					}
 				}
-			}
-		};
+			};
 
-		$stateProvider.state('home.inventory', inventory);
-	});
+
+			var left = {
+				url: '/',
+				views: {
+					'main@home' : {
+						controller : 'homeCtrl',
+						templateUrl: 'scripts/home/partials/home.html'
+					}
+				}
+			};
+
+			$stateProvider.state('home.inventory', inventory);
+			$stateProvider.state('home.inventory.left', left);
+
+
+		});
 }());
