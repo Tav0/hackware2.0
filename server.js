bce8e0eeb27aa46b936659a0f9c037d156688a0c
penguinologist <jgem2u2@gmail.com>
2016-02-24 19:13:04 -0500

    fixed a ton

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 (function() {
 	'use strict';
 
-<<<<<<< HEAD
-	var express      = require('express'),
-		bodyParser     = require('body-parser'),
-		app            = express(),
-		errorHandler   = require('errorhandler'),
-		methodOverride = require('method-override'),
-		hostname       = process.env.HOSTNAME || '127.0.0.1',
-		port           = parseInt(process.env.PORT, 10) || 9999,
-		publicDir      = process.argv[2] || __dirname + '/';
-=======
+
 	var express        = require('express'),
 			bodyParser     = require('body-parser'),
 			app            = express(),
@@ -19,7 +10,7 @@
 			hostname       = process.env.HOSTNAME || '127.0.0.1',
 			port           = parseInt(process.env.PORT, 10) || 9999,
 			publicDir      = process.argv[2] || __dirname + '/';
->>>>>>> Jeroen
+
 
 	app.use(methodOverride());
 	app.use(bodyParser.json()); //support json encoded bodies
